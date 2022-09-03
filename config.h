f6e3ceffd53cdd5b4cac508db99771101914d144
runiales <irpic.am@gmail.com>
2022-09-03 20:11:45 +0200

    thinkpad

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Fira Code Medium:size=15", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -32,7 +32,7 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "100x34", NULL };
+const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
@@ -216,11 +216,6 @@ static Key keys[] = {
 	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
-<<<<<<< HEAD
-	{ MODKEY,			XK_c,		spawn,		SHCMD(TERMINAL " -e wyrd") },
-	{ MODKEY,			XK_v,		spawn,		SHCMD(TERMINAL " -e taskwarrior-tui") },
-=======
->>>>>>> 07a94df (preinstall)
 	{ MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 	{ MODKEY,			XK_v,		spawn,		SHCMD(TERMINAL " -e taskwarrior-tui") },
 	{ MODKEY|ShiftMask,		XK_v,		spawn,		SHCMD("ugrvpn") },
@@ -240,13 +235,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_Page_Down,	shifttag,	{ .i = +1 } },
 	{ MODKEY,			XK_Insert,	spawn,		SHCMD("xdotool type $(cat ~/.local/share/larbs/snippets | dmenu -i -l 50 | cut -d' ' -f1)") },
 
-<<<<<<< HEAD
-	/*{ MODKEY,			XK_F1,		spawn,		SHCMD("") },*/
-	{ MODKEY,			XK_F2,		spawn,		SHCMD("tutorialvids") },
-=======
 	/* { MODKEY,			XK_F1,		spawn,		SHCMD("") }, */
 	/*{ MODKEY,			XK_F2,		spawn,		SHCMD("") },*/
->>>>>>> 07a94df (preinstall)
 	{ MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
 	{ MODKEY,			XK_F4,		spawn,		SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
